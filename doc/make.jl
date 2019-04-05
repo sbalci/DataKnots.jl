b4487d656a499777908018359e310788dbc8a3f6
Clark Evans <cce@clarkevans.com>
2019-04-05 11:36:20 -0400

    resuscitate patient data simulation

--- doc/make.jl
+++ doc/make.jl
@@ -2,9 +2,19 @@
 
 using Pkg
 haskey(Pkg.installed(), "Documenter") || Pkg.add("Documenter")
+haskey(Pkg.installed(), "Literate") || Pkg.add("Literate")
+haskey(Pkg.installed(), "Random") || Pkg.add("Random")
+haskey(Pkg.installed(), "Distributions") || Pkg.add("Distributions")
+haskey(Pkg.installed(), "Plots") || Pkg.add("Plots")
 
 using Documenter
 using DataKnots
+using Literate
+
+# Convert Literate example code to markdown.
+INPUTS = joinpath(@__DIR__, "src", "simulation.jl")
+OUTPUT = joinpath(@__DIR__, "src")
+Literate.markdown(INPUTS, OUTPUT, documenter=true, credit=false)
 
 # Highlight indented code blocks as Julia code.
 using Markdown
@@ -26,6 +36,9 @@ makedocs(
             "shapes.md",
             "knots.md",
             "queries.md",
+        ],
+        "Showcase" => [
+            "simulation.md",
         ],
     ],
     modules = [DataKnots])
